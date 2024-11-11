a6a376b4622cd318f484a507d987129a37caa895
Ahmet Kutay URAL <69294802+akutayural@users.noreply.github.com>
2024-11-11 00:56:04 +0000

    setup.py has been updated.

--- setup.py
+++ setup.py
@@ -1,22 +1,35 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# Read the contents of your README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="APIException",
-    version="0.1.0",
-    author="Ahmet Kutay Ural",
-    author_email="ahmetkutayural@gmail.com",
+    version="0.1.1",
     description="A customizable exception handling library for FastAPI",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/akutayural/APIException",  # Replace with your GitHub repo
-    packages=find_packages(exclude=["test", "venv"]),
+    author="Ahmet Kutay URAL",
+    packages=find_packages(),
     install_requires=[
-        "pydantic>=2.0.0"
+        "pydantic>=2.0.0",
     ],
+    python_requires=">=3.6",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    python_requires=">=3.7",
+    project_urls={
+        "Documentation": "https://github.com/akutayural/APIException",
+        "Source": "https://github.com/akutayural/APIException",
+        "PyPI": "https://pypi.org/project/APIException/",
+    },
 )
