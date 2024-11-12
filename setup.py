acf0d9be3890ce19f3ade4bebca0b6ff61f325b7
Ahmet Kutay URAL <69294802+akutayural@users.noreply.github.com>
2024-11-12 11:40:37 +0000

    Examples has been added. Fastapi usage has been added. Also fastapi and uvicorn has been added in requirements.txt.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="APIException",
-    version="0.1.2",
+    version="0.1.4",
     description="A customizable exception handling library for FastAPI",
     long_description=long_description,
     long_description_content_type="text/markdown",
